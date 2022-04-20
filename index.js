c02419db68f879c4ea492dfadd5c5c55fb0107b2
Jair montenegro <jairmontenegrof@gmail.com>
2022-04-20 02:56:50 -0300

    modification

---
+++ index.js
@@ -0,0 +1,11 @@
+const buttonNumber = document.querySelectorAll('[data-num]')
+const buttonOperator = document.querySelectorAll('[data-operator]')
+const buttonEqual = document.querySelector('[data-equal]')
+const buttonAllClear = document.querySelector('[data-allclear]')
+const buttonDelete = document.querySelector('[data-delete]')
+const upText = document.getElementById('value-1')
+const downText = document.getElementById('value-2')
+
+// mouse 
+
+
